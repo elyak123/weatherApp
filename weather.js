e6260908a61c060c96d37b417e06f392acaf9d59
elyak123 <elyak.123@gmail.com>
2016-03-01 05:28:22 -0600

    Debuggeo

--- weather.js
+++ weather.js
@@ -2,15 +2,20 @@ $(document).ready(function(){
 	var rawUrl = "api.openweathermap.org/data/2.5/weather?";
 	var key = "4d1ae4b1501eb1f48985bd4fe68b5233";
 	navigator.geolocation.getCurrentPosition(function(pos){
-		$.ajax({
-			url: rawUrl + "lat=" + pos.latitude + "&" + pos.longitude + "&appid=" + key,
-			method: "GET",
-			dataType: "jsonp",
-			success: function(x){
-				$("#weather").append(x);
-				console.log(x);
-			}
-		});		
-	})
+		$("#weather").append(pos);
+	});
 
+
+
+	// navigator.geolocation.getCurrentPosition(function(pos){
+	// 	$.ajax({
+	// 		url: rawUrl + "lat=" + pos.latitude + "&" + pos.longitude + "&appid=" + key,
+	// 		method: "GET",
+	// 		dataType: "jsonp",
+	// 		success: function(x){
+	// 			$("#weather").append(x);
+	// 			console.log(x);
+	// 		}
+	// 	});		
+	// })
 });
