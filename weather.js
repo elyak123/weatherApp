c84d27b71dd275b1453b0e551cf5cae9f8b76ada
elyak123 <elyak.123@gmail.com>
2016-03-04 06:27:04 -0600

    prueba

--- weather.js
+++ weather.js
@@ -1,27 +1,5 @@
 $(document).ready(function(){
-	// debugger;
-	// $.get('http://elyak123.github.io/Portafolio-fcc/Templates/encabezado.html', function (data){
-	// 	$('header').html(data);
-	// });
-	// debugger;
-	$(".boton-movil").click(function(){
-		debugger;
-		if ($("li").hasClass("menu-listitem")) {
-			console.log("true");
-			$(".menu-listitem").slideDown("slow").addClass("menu-listMovil").removeClass("menu-listitem");	
-		} else{
-			console.log("false");
-			$(".menu-listMovil").slideUp("slow").addClass("menu-listitem");
-			//$(".menu-listitem").removeClass("menu-listMovil");
-		};
-		
-		console.log("click");
-		$(window).on("resize", function () {
-			if ($(window).width() > 500) {
-	    		$("li").css('display','').removeClass("menu-listMovil");
-			}
-		}).resize();
-	});
+	encabezado();
 	var rawUrl = "http://api.openweathermap.org/data/2.5/weather?";
 	var key = "4d1ae4b1501eb1f48985bd4fe68b5233";
 	$.getJSON('http://ipinfo.io', function(data){
