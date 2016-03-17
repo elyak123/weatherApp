bfa79d4badd5f3454a048950de9d544d94514312
elyak123 <elyak.123@gmail.com>
2016-03-17 06:23:03 -0600

    debuggeo

--- weather.js
+++ weather.js
@@ -3,47 +3,47 @@ $(document).ready(function(){
 	var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?";
 	var currentUrl = "http://api.openweathermap.org/data/2.5/weather?";
 	var key = "4d1ae4b1501eb1f48985bd4fe68b5233";
-	// $.getJSON('http://ipinfo.io', function(data){
-	// 	//console.log(data);
-	// 	var split = data.loc.split(',');
-	// 	var lat = split[0];
-	// 	var lon = split[1];
-	// 	$.ajax({
-	// 		url: currentUrl + "lat=" + lat + "&lon=" +lon + "&lang=es&units=metric&appid=" + key,
-	// 		method: "GET",
-	// 		dataType: "jsonp",
-	// 		success: function(x){
-	// 			var icon = x.weather[0].icon;
-	// 			var daylight = icon.slice(2,2);
-	// 			if (daylight === "d") {
-	// 				$('#animation').addClass('day');
-	// 			} else{
-	// 				$('#animation').addClass('night');
-	// 				$('#current').addClass('currentNight');
-	// 			}
-	// 			$('#currentImage').attr('src','weathericons/' + icon + '.png').attr('alt', x.weather[0].description);
-	// 			$('#currentDescription').html(x.weather[0].description);
-	// 			$('#currentTemp span').html(Math.round(x.main.temp));
-	// 			$('#city').html(x.name);
-	// 			//hacer un switch con nueve casos para describir las distintas animaciones.
-	// 			console.log(x);
-	// 			//debugger;
-	// 		}
-	// 	});
-	// 	// $.ajax({
-	// 	// 	url: forecastUrl + "lat=" + lat + "&lon=" +lon + "&lang=es&units=metric&appid=" + key,
-	// 	// 	method: "GET",
-	// 	// 	dataType: "jsonp",
-	// 	// 	success: function(y){
-	// 	// 		var icon 		= y.list[0].weather[0].icon;
-	// 	// 		var description = y.list[0].weather[0].description;
-	// 	// // 		$('#currentImage').attr('src','weathericons/' + icon + '.png').attr('alt', description);
-	// 	// // 		$('#currentTemp').html(y.list.temp);
-	// 	// // 		$('#city').html(y.name);				
-	// 	// 		console.log(y);
-	// 	// 	}
-	// 	// });
-	// });
+	$.getJSON('http://ipinfo.io', function(data){
+		//console.log(data);
+		var split = data.loc.split(',');
+		var lat = split[0];
+		var lon = split[1];
+		$.ajax({
+			url: currentUrl + "lat=" + lat + "&lon=" +lon + "&lang=es&units=metric&appid=" + key,
+			method: "GET",
+			dataType: "jsonp",
+			success: function(x){
+				var icon = x.weather[0].icon;
+				var daylight = icon.slice(2,2);
+				if (daylight === "d") {
+					$('#animation').addClass('day');
+				} else{
+					$('#animation').addClass('night');
+					$('#current').addClass('currentNight');
+				}
+				$('#currentImage').attr('src','weathericons/' + icon + '.png').attr('alt', x.weather[0].description);
+				$('#currentDescription').html(x.weather[0].description);
+				$('#currentTemp span').html(Math.round(x.main.temp));
+				$('#city').html(x.name);
+				//hacer un switch con nueve casos para describir las distintas animaciones.
+				console.log(x);
+				//debugger;
+			}
+		});
+		// $.ajax({
+		// 	url: forecastUrl + "lat=" + lat + "&lon=" +lon + "&lang=es&units=metric&appid=" + key,
+		// 	method: "GET",
+		// 	dataType: "jsonp",
+		// 	success: function(y){
+		// 		var icon 		= y.list[0].weather[0].icon;
+		// 		var description = y.list[0].weather[0].description;
+		// // 		$('#currentImage').attr('src','weathericons/' + icon + '.png').attr('alt', description);
+		// // 		$('#currentTemp').html(y.list.temp);
+		// // 		$('#city').html(y.name);				
+		// 		console.log(y);
+		// 	}
+		// });
+	});
 	$('#gpsTrigger').on('click', function(){
 		navigator.geolocation.getCurrentPosition(function(pos){
 			var lat = pos.coords.latitude;
